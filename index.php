ea6e585195b8b0cfe8e267d461ca913fbcb44345
Na-naO <la.dolce.vita.nana@gmail.com>
2021-01-14 19:31:59 +0900

    Connect to css

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <title>Portfolio</title>
 </head>
 <body>
@@ -11,6 +12,12 @@
 
 
 <main>
+<section class="concept">
+<h1>Concept</h1>
+</section>
+
+
+
 <h1>About me</h1>
 </main>
 
@@ -20,3 +27,4 @@
   
 </body>
 </html>
+
