b9b121c1f71dae9edfe18f10075f828e355e46bb
Na-naO <la.dolce.vita.nana@gmail.com>
2021-05-05 08:31:17 +0900

    add skills section

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Portfolio</title>
+  <link href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" rel="stylesheet">
   <link rel="stylesheet" href="assets/style.css">
 </head>
 
@@ -26,37 +27,73 @@
     <div class="container">
       <h1>ご覧いただきありがとうございます</h1>
       <p>my portfolio</p>
-
-
     </div>
-    <!-- <img src="img/pexels-vincent-gerbouin-1168764.jpg" alt="" class="header"> -->
-    
   </div>
 
 
+  <div class="main-wrapper">
 
-  <main>
     <div class="concept">
       <h1>Concept</h1>
     </div>
 
-    <div class="service">
-      <h1>Service</h1>
+    <div class="skills">
+      <div class="skills-heading">
+        <h1>Skills</h1>
+      </div>
+      <div class="skills-wrapper">
+        <div class="skills-container">
+          <div class="skills-icons">
+            <i class="fab fa-html5 size"></i>
+            <p></p>
+          </div>
+          <div class="skills-contents"></div>
+        </div>
+        <div class="skills-container">
+          <div class="skills-icons">
+            <i class="fab fa-css3-alt size"></i>
+            <p></p>
+          </div>
+          <div class="skills-contents"></div>
+        </div>
+        <div class="skills-container">
+          <div class="skills-icons">
+            <i class="fab fa-php size"></i>
+            <p></p>
+          </div>
+          <div class="skills-contents"></div>
+        </div>
+        <div class="skills-container">
+          <div class="skills-icons">
+            <i class="fab fa-wordpress size"></i>
+            <p></p>
+          </div>
+          <div class="skills-contents"></div>
+        </div>
+        <div class="skills-container">
+          <div class="skills-icons">
+            <i class="fab fa-shopify size"></i>
+            <p></p>
+          </div>
+          <div class="skills-contents"></div>
+        </div>
+      </div>
     </div>
 
     <div class="works">
       <h1>Works</h1>
-      <div class="card">
+      <!-- <div class="card">
         <img src="img/pexels-pok-rie-4856367.jpg" alt="" class="card_img">
         <div class="card_content">
           <h1 class="card_title">ポートフォリオ①</h1>
           <p class="card_text">最初のポートフォリオ</p>
         </div>
-      </div>
+      </div> -->
+    </div>
 
-      <div class="about_me">
-        <h1>About me</h1>
-      </div>
+
+    <div class="about_me">
+     <h1>About me</h1>
     </div>
 
 
@@ -86,13 +123,12 @@
           <div class="contact_content">
             お問い合わせ内容
             <textarea name="content" cols="40" rows="10"></textarea>
+            <p>まずはお気軽にご相談ください！</p>
           </div>
         </form>
       </div>
     </div>
-
-  </main>
-
+  </div>
 
   <footer>
   </footer>
